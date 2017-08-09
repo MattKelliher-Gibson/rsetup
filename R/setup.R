19fdf35d9dfd843277be96cdf417727a92a0b140
Matthew Kelliher-Gibson <mkg@mattkellihergibson.com>
2017-08-09 13:59:22 -0400

    Fix bug and change default libraries
    - Add explicit package for functions
    - Fixed missing ")"
    - Fixed spelling errors
    - Changed library readr to rio
    - Changed library RODBC to DBI and odbc
    - Added function file

--- R/setup.R
+++ R/setup.R
@@ -1,26 +1,31 @@
 create_setup <- function() {
   f.code <- prepr::file_location("/Code")
   f.r <- prepr::file_location("/Code/R")
-  f.setup <- file_location("/Code/R/P000_Setup.r")
+  f.setup <- prepr::file_location("/Code/R/P000_Setup.r")
+  f.function <- prepr::file_location("/Code/R/P005_Functions.r")
+  f.files <- prepr::file_location("/Files")
 
   i.options <- c("#############", "#I. Options #", "#############", "",
                  "#A. Memory Size", "", "\tmemory.limit(size = NA)", "",
-                 "#B. stringsAsFactors", "", "\toptions(stringsAsFactors = FALSE)", "")
+                 "#B. stringsAsFactors", "", "\toptions(stringsAsFactors = FALSE)", "",
 
   ii.packages <- c("###############", "#II. Packages #", "###############\n",
                    "#A. prepr\n", "\tlibrary(prepr)\n",
-                   "#B. readr\n", "\tlibrary(readr)\n",
+                   "#B. rio\n", "\tlibrary(rio)\n",
                    "#C. tibble\n", "\tlibrary(tibble)\n",
-                   "#D. dplyr\n", "\tlibary(dplyr)\n",
+                   "#D. dplyr\n", "\tlibrary(dplyr)\n",
                    "#E. tidyr\n", "\tlibrary(tidyr)\n",
                    "#F. purrr\n", "\tlibrary(purrr)\n",
-                   "#G. RODBC\n", "\tlibrary(RODBC)\n")
+                   "#G. DBI\n", "\tlibrary(DBI)\n",
+                   "#H. obdc\n", "\tlibrary(odbc)\n")
 
   iii.variables <- c("#################", "#III. Variables #", "#################\n",
                      "#A. File Location\n", "\t#1. Import", "\t", "\t#2. Export\n",
                      "#B. Constant\n", "\t#1. SQL Query", "\t", "\t#2. Data Source Name\n")
 
   k.setup <- c(i.options, ii.packages, iii.variables)
+
+  k.function <- c("#A. ", "\n")
 
   if(!dir.exists(f.code)) {
     dir.create(f.code)
@@ -29,6 +34,11 @@ create_setup <- function() {
     dir.create(f.r)
   }
 
+  if(!dir.exists(f.files)) {
+    dir.create(f.files)
+  }
+
   readr::write_lines(k.setup, f.setup)
-  file.edit(f.setup)
+  readr::write_lines(k.function, f.function)
+  file.edit(c(f.setup, f.function))
 }
